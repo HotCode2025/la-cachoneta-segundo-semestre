4081e81d7ee6f9273143bc55f5dc56bdc870c79f
Emmanuel Yapura <eyapura96@gmail.com>
2025-08-26 18:33:50 -0300

    carga codigo del video 3 clase 2 javascript

--- Javascript/TSLeccion02/02-03-arreglos.js
+++ Javascript/TSLeccion02/02-03-arreglos.js
@@ -13,3 +13,19 @@ console.log(autos[2]);
 for(let i = 0; i < autos.length; i++){
 console.log(i + " : " +autos[i])
 }
+
+// modificamos los elementos del arreglo
+autos[1] = 'Volvo';
+console.log(autos[1]);
+
+// agregamos nuevos valores al arreglo
+autos.push('Audi'); // agregamos el elemento al final del arreglo
+console.log(autos);
+
+// otras formas de agregar elementos al arreglo
+autos[autos.length] = 'Porche';
+console.log(autos);
+
+// tercera forma de agregar elementos teniendo cuidado
+autos[6] = 'Renault';
+console.log(autos);
