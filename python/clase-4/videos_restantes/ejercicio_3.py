1fb2a1fad2a7873945f90595dbbb8ced5e0abddd
Emmanuel Yapura <eyapura96@gmail.com>
2025-09-09 04:50:25 -0300

    agrega videos faltantes de la clase 4

---
+++ python/clase-4/videos_restantes/ejercicio_3.py
@@ -0,0 +1,18 @@
+# ejercicio 3: insertar elementos y ordenarlos
+# pedir numeros y meterlos en una lista, cuando el usuario
+# introduzca un numero 0, nuestro programa dejaria de insertar.
+# Por ultimo, mostrar los numeros ordenados de menor a mayor
+
+lista = []
+salir = False
+
+while not salir: 
+    numero = int(input('Digite un numero: '))
+    if numero == 0:
+        salir = True
+    else: 
+        lista.append(numero)
+
+lista.sort() # la lista esta ordenada con esta funcion
+print(f' \n Lista ordenada: \n{lista}')
+    
---
+++ python/clase-4/videos_restantes/main.py
@@ -0,0 +1,8 @@
+# comenzamos con funciones
+# mi_funcion() # no se puede llamar antes de definir a una funcion
+# definimos una funcion
+def mi_funcion(): # para identificar a la funcion utilizamos parentesis
+    print('Saludos a todos los alumunos de la tecnitura ')
+    
+mi_funcion() # estamos llamando a la funcion 
+mi_funcion() # se puede llamar a una funcion N cantidad de veces 
