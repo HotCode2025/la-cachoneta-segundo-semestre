af5637118e71f5391e670635c2b37e83aef9dc0f
EmmanuelYapura <85853405+EmmanuelYapura@users.noreply.github.com>
2025-10-18 03:58:32 -0300

    agrega metodo toString

--- Java/Clase8/Clase8Tarea/src/Clases/Persona.java
+++ Java/Clase8/Clase8Tarea/src/Clases/Persona.java
@@ -32,4 +32,9 @@ public class Persona {
 	public void setEliminado(boolean eliminado) {
 		this.eliminado = eliminado;
 	}
+	
+	public String toString() { // convierte en una cadena cada atributo
+        return "Persona [nombre=" + this.nombre + ", sueldo=" + this.sueldo + ", eliminado=" + this.eliminado + "]";
+    }
 }
+
