1d1c6586bda2b768d6612adc87fb48a23e2a76ca
EmmanuelYapura <85853405+EmmanuelYapura@users.noreply.github.com>
2025-10-17 22:56:45 -0300

    Crea clase Aritmetica

---
+++ Java/clase4/Operaciones/Aritmetica.java
@@ -0,0 +1,14 @@
+package Operaciones;
+
+public class Aritmetica {
+    // atributos de la clase
+    int a;
+    int b;
+    
+    // metodo 
+
+    public void sumarNumeros (){
+        int resultado = a + b;
+        System.out.println("resultado: " + resultado);
+    }
+}
