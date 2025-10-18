ebf222a533c0d34c87c240e6a8b40b25ce1d3317
EmmanuelYapura <85853405+EmmanuelYapura@users.noreply.github.com>
2025-10-17 22:55:16 -0300

    Crea la clase Persona con sus atributos y metodos

---
+++ Java/clase4/Clases/Persona.java
@@ -0,0 +1,13 @@
+package Clases;
+
+public class Persona{
+    // atributos de la clase (caracteristicas)
+    String nombre;
+    String apellido;
+
+    // metodos de la clase (acciones)
+    public void obtenerInformacion(){
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Apellido: " + apellido);
+    }
+}
