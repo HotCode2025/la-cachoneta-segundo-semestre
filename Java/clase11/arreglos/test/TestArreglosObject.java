4c9d6262d745a16470b655dc59b1a0393ae2254c
EmmanuelYapura <85853405+EmmanuelYapura@users.noreply.github.com>
2025-11-02 01:22:26 -0300

    agrega clase 11

---
+++ Java/clase11/arreglos/test/TestArreglosObject.java
@@ -0,0 +1,21 @@
+package test;
+
+public class TestArreglosObject {
+    public static void main(String[] args) {
+        Persona personas[] = new Persona[2];
+        personas [0] = new Persona("Ariel"); 
+        personas [1] = new Persona("Osvaldo");
+        System.out.println("personas 0 = " + personas[0]); 
+        System.out.println("personas 1 = " + personas[1]); 
+
+        for(int i = 0; i < personas.length; i++){
+            System.out.println("personas "+ i + " = " + personas[i]);
+        }
+
+        // trabajamos con arreglso en la sintaxis resumida
+        String frutas [] = { "banana", "pera", "durazno"};
+        for(int i = 0; i < frutas.length; i++){
+            System.out.println("frutas "+ i + " = " + frutas[i]);
+        }
+    }   
+}
